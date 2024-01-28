dbd388cd104105547dec64cf9ad656eb81eb5a07
PierreMrt <merlet.pierre@outlook.com>
2024-01-28 15:35:25 +0100

    corrected syntax error in settings.py...

--- web_portfolio/settings.py
+++ web_portfolio/settings.py
@@ -104,10 +104,8 @@ if DEBUG:
             }
 else:
     DATABASES = {
-        'default': dj_database_url.config(
-                # Feel free to alter this value to suit your needs.
-                default='postgresql://postgres:postgres@localhost:1000/webportfolio',
-                conn_max_age=600    )}
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+                }
 
 
 

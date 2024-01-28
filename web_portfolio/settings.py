ac9a04620ae893596cac7cb8438c09019b42b332
PierreMrt <merlet.pierre@outlook.com>
2024-01-28 11:53:30 +0100

    prepared for production

--- web_portfolio/settings.py
+++ web_portfolio/settings.py
@@ -34,6 +34,10 @@ ALLOWED_HOSTS = ['localhost',
                 '127.0.0.1',
                 '.com']
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:    
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 CSRF_TRUSTED_ORIGINS = ['https://*.com']
 
 
